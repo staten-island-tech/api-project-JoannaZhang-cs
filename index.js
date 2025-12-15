5bbfbca150074dc2e07a367e2c3a2ec740c08feb
JoannaZhang-cs <jzsunset10@gmail.com>
2025-12-15 12:32:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ async function getData(){
         const response = await fetch(//link from api, use back tics, ${value} at the end);
         if (response.status != 200){
             throw new Eror (response);
+        }else{
+            const data = await response.json()
+            //makes the response into json (java script object notation) data we can use
+            console.log(data);
+            document.getElementById("api-response").textContent = data.name;
+
         }
     } catch (error) {
         console.log(error);
 
     }
 }
+getData("//item")
+
+
+//looking and testing api project 
+
+//arrays equals for loop
+//objects is different
+
+//resources for layout: tailwindcss - flowbite & daistUI (on top fo tailwindcss)
+//codepen: search for css cards
