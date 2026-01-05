9b767e2be023ffc6b18c0b0e9415b35dc7e26aa0
JoannaZhang-cs <jzsunset10@gmail.com>
2026-01-05 12:32:30 -0500

    1/5

--- poke.js
+++ poke.js
@@ -1,7 +1,7 @@
 async function getData(){
     try{
         //go get data
-        const response = await fetch(//link from api, use back tics, ${value} at the end);
+        const response = await fetch()//link from api, use back tics, ${value} at the end);
         if (response.status != 200){
             throw new Eror (response);
         }else{
